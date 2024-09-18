ee8c49d4755a7cd9a8ee867490d94a0af8f79ad2
Demonware023 <awamchimere@icloud.com>
2024-09-18 07:15:25 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,9 @@
-// Display welcome message and ask for the user's name
-console.log("Welcome to Holberton School, what is your name?");
+// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Capture the user's input from stdin
 process.stdin.on('data', (data) => {
-  // Trim any extra whitespace from input
-  const name = data.toString().trim();
-  
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-
-  // Close the program after receiving the input
-  process.exit();
-});
-
-// When the program is about to close, display the closing message
-process.on('exit', () => {
-  console.log('This important software is now closing');
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
 });
